4104fcdb8893914f63afced7a0f4bf36c227f798
Hana Lee <hlee@cloudpassage.com>
2017-03-17 17:49:03 -0700

    optimized cut method

--- lib/controllers/version_comparator.py
+++ lib/controllers/version_comparator.py
@@ -3,10 +3,6 @@ import re
 
 class VersionComparator(object):
     @staticmethod
-    def remove_arch(s):
-        return s.replace('.x86_64', '')
-
-    @staticmethod
     def evr(s):
         found_el = re.search('el\d+', s)
         found_fc = re.search('fc\d+', s)
@@ -15,25 +11,14 @@ class VersionComparator(object):
         elif found_fc:
             return found_fc.group(0)
 
-    @staticmethod
-    def normalize(version):
-        nums = []
-        for v in version:
-            try:
-                nums.append(int(v))
-            except:
-                pass
-        return nums
-
     def cut(self, s):
-        s = self.remove_arch(s)
-        return re.split('\.|-', s)
+        return re.split('\.|-', re.split('.[a-z]', s)[0])
 
     def partition_comparison(self, a, b):
         for i in range(len(a)):
             try:
-                if (a[i] - b[i]) > 0:
-                    return a[i] > b[i]
+                if (int(a[i]) - int(b[i])) > 0:
+                    return int(a[i]) > int(b[i])
             except IndexError:
                 return True
         return False
@@ -41,8 +26,8 @@ class VersionComparator(object):
     def compare(self, a, b):
         evr_a = self.evr(a)
         evr_b = self.evr(b)
-        a = self.normalize(self.cut(a))
-        b = self.normalize(self.cut(b))
+        a = self.cut(a)
+        b = self.cut(b)
         if not evr_a or not evr_b:
             return self.partition_comparison(a, b)
         if evr_a == evr_b:
